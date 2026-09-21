fd9e539754ce10f8839888d15c9ae313fad6a780
itsprakriti1234-tech <itsprakriti1234@gmail.com>
2026-09-21 21:10:15 +0530

    Add files via upload
    Uploaded basic python programs ,
    1. Sum of Two Numbers 
    2. String Reversal

---
+++ python code 1.py
@@ -0,0 +1,4 @@
+x = 100
+y = 500
+result = x + y
+print(result)
---
+++ python code 2.py
@@ -0,0 +1,8 @@
+name = ["Sujata", "Pallavi", "Anita"]
+number = [1,2,3,4,5,6]
+print("List of numbers: ",numbers)
+
+str="Bootcamp on AI"
+lst = str.split()
+for i  in range (len(lst)-1, -1, -1):
+    print(lst[i])
