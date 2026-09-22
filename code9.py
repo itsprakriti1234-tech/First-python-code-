9e260bf23757797658e1ce8d3da35a90b850be10
Prakriti <itsprakriti1234@gmail.com>
2026-09-23 00:19:15 +0530

    Delete code9.py

--- code9.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import numpy as np
-data={
-    'visitor_ID': [f'V{1000+i}' for i in range(20)],
-    'Name':['Abhishek Sandeep Zade', 'Ashwini Lalchand Mundaware', 'Gayatri Suresh Gayekwad','Harshada Ganesh Chaudhary', 
-
-                  
